7b5f6be5f21771adffa25cc9730f2e0abaea5291
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-08 00:34:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ echo "[INFO] Running com.github.noraui.gherkin.GherkinConditionedLoopedStepUT"
 
 curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 
-counterFailures=$(sed -n 's:.*\[INFO\] Tests run: 4, Failures: \(.*\), , Errors: 0, Skipped: 0, Time elapsed: 7.16 s - in com.github.noraui.browser.AuthUT.*:\1:p' nonaui.log | head -n 1)
-echo "******** counterFailures: $counterFailures"
+
