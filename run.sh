76484986f820726f07c839dbe57db0590effd7cc
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-08 00:25:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "[1565214900.443][SEVERE]: Timed out receiving message from renderer: -0.011"
 echo "[INFO] Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 7.16 s - in com.github.noraui.browser.AuthUT"
 echo "[INFO] Running com.github.noraui.gherkin.GherkinConditionedLoopedStepUT"
