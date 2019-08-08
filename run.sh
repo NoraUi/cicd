392b8c2974b5ab28f4d84c8c09bc58624d428336
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-08 12:54:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "[1565214900.443][SEVERE]: Timed out receiving message from renderer: -0.011" > nonaui.log
-echo "[INFO] Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 7.16 s - in com.github.noraui.browser.AuthUT" > nonaui.log
-echo "[INFO] Running com.github.noraui.gherkin.GherkinConditionedLoopedStepUT" > nonaui.log
+curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 
-
+echo "[1565214900.443][SEVERE]: Timed out receiving message from renderer: -0.011" >> nonaui.log
+echo "[INFO] Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 7.16 s - in com.github.noraui.browser.AuthUT" >> nonaui.log
+echo "[INFO] Running com.github.noraui.gherkin.GherkinConditionedLoopedStepUT" >> nonaui.log
 
 counterFailures=$(sed -n 's/.*\[INFO\] Tests run: 4, Failures:\(.*\), Errors: 0, Skipped: 0, Time elapsed.*/\1/p' nonaui.log | head -n 1)
 echo "******** counter: >$counterFailures<"
